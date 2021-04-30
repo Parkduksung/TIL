ef3c3b159dc222ab8f352b8086593c7f1af43883
Parkduksung <duksung1234@naver.com>
2021-04-30 10:57:37 +0900

    [Clipboard] 리스너 따로 변수화 하여 빼놓기도 가능.

--- app/src/main/java/com/example/til/clipboard/ClipboardActivity.kt
+++ app/src/main/java/com/example/til/clipboard/ClipboardActivity.kt
@@ -8,15 +8,17 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.til.R
 
-class ClipboardActivity : AppCompatActivity(), ClipboardManager.OnPrimaryClipChangedListener {
+class ClipboardActivity : AppCompatActivity() {
 
     private val clipboard by lazy { Clipboard() }
+
+    private val clipChangedListener = ClipboardManager.OnPrimaryClipChangedListener { Log.d("결과", "복사됨...") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clipboard)
 
-        clipboard.setClipboardListener(this, this)
+        clipboard.setClipboardListener(this, clipChangedListener)
 
         findViewById<Button>(R.id.clipboard_btn).setOnClickListener {
             Toast.makeText(this, clipboard.getClipboardText(this) ?: "널임", Toast.LENGTH_LONG).show()
@@ -24,7 +26,4 @@ class ClipboardActivity : AppCompatActivity(), ClipboardManager.OnPrimaryClipCha
 
     }
 
-    override fun onPrimaryClipChanged() {
-        Log.d("결과", "복사됨...")
-    }
 }
