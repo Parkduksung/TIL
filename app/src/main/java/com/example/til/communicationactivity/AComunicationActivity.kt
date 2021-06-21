6bd9a5dbe5ae634d7b7ee70e31963834d961477e
Parkduksung <duksung1234@naver.com>
2021-06-21 19:15:11 +0900

    [CommunicationActivity] 기초 세팅.

---
+++ app/src/main/java/com/example/til/communicationactivity/AComunicationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.til.communicationactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.til.R
+
+class ACommunicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_communicate_a)
+    }
+}
---
+++ app/src/main/java/com/example/til/communicationactivity/CComunicationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.til.communicationactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.til.R
+
+class CCommunicationActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_communicate_c)
+    }
+}
