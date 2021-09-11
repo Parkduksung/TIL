c480a359e55efb9ae49c209639ac607878e5e717
Parkduksung <duksung1234@naver.com>
2021-09-11 17:41:03 +0900

    [리사이클러뷰와리스트뷰] ListView, 매번 계속 생성시에는 itemCount 에 따라 계속적으로 화면을 생성.

--- app/src/main/java/com/example/til/comparelistviewandrecyclerview/ListViewActivity.kt
+++ app/src/main/java/com/example/til/comparelistviewandrecyclerview/ListViewActivity.kt
@@ -1,13 +1,34 @@
 package com.example.til.comparelistviewandrecyclerview
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.til.R
 
 class ListViewActivity : AppCompatActivity() {
 
+    private val listview: ListView by lazy { findViewById(R.id.listview) }
+
+    private val listViewAdapter = ListViewAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listview)
+
+
+        listview.run {
+            adapter = listViewAdapter
+        }
+
+        listViewAdapter.addAll(mockItemList)
+    }
+
+    companion object {
+        val mockItemList = mutableListOf<String>().apply {
+            for (i in 0..30) {
+                add("---$i 번째 힌트입니다.---")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/til/comparelistviewandrecyclerview/ListViewAdapter.kt
+++ app/src/main/java/com/example/til/comparelistviewandrecyclerview/ListViewAdapter.kt
@@ -1,6 +1,43 @@
 package com.example.til.comparelistviewandrecyclerview
 
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.EditText
+import com.example.til.R
 
 class ListViewAdapter : BaseAdapter() {
+
+    private val itemList = mutableListOf<String>()
+
+    fun addAll(list: List<String>) {
+        itemList.addAll(list)
+    }
+
+    override fun getCount(): Int =
+        itemList.size
+
+    override fun getItem(position: Int): String =
+        itemList[position]
+
+    override fun getItemId(position: Int): Long =
+        position.toLong()
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+
+        Log.d("결과", "$position 불림.")
+
+        val layoutInflater =
+            LayoutInflater.from(parent?.context).inflate(R.layout.item_listview, parent, false)
+
+        val editText: EditText = layoutInflater.findViewById(R.id.edittext)
+
+        editText.hint = itemList[position]
+
+        return layoutInflater
+    }
 }
