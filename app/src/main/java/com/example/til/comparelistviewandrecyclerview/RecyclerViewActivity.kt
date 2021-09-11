d5d385def031fa50753bfc9c23a90b5ae5ae5fc5
Parkduksung <duksung1234@naver.com>
2021-09-11 16:00:35 +0900

    [리사이클러뷰와리스트뷰] Activity 생성.

---
+++ app/src/main/java/com/example/til/comparelistviewandrecyclerview/RecyclerViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.til.comparelistviewandrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.til.R
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview)
+    }
+}
