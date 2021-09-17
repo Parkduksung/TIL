e0ab0e3b9af8edb2be7df772375884b9ea2ee663
Parkduksung <duksung1234@naver.com>
2021-09-17 20:50:02 +0900

    [url svg] 기본 세팅 및 dependency 추가.

---
+++ app/src/main/java/com/example/til/url/UrlSGVActivity.kt
@@ -0,0 +1,18 @@
+package com.example.til.url
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.til.R
+import java.io.ByteArrayInputStream
+
+class UrlSGVActivity : AppCompatActivity(R.layout.activity_url_svg) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
