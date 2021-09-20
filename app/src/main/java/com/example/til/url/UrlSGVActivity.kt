bca19224176deef8c77cf73b6a81918ae32261ac
Parkduksung <duksung1234@naver.com>
2021-09-21 01:20:39 +0900

    glide 사용했지만 별로인거 좀 더 분석이 필요해 보인다..

--- app/src/main/java/com/example/til/url/UrlSGVActivity.kt
+++ app/src/main/java/com/example/til/url/UrlSGVActivity.kt
@@ -1,24 +1,13 @@
 package com.example.til.url
 
 import android.graphics.drawable.PictureDrawable
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Looper
-import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.caverock.androidsvg.SVG
-import com.caverock.androidsvg.SVGImageView
-import com.caverock.androidsvg.SVGParser
 import com.example.til.R
-import kotlinx.coroutines.MainScope
-import java.io.ByteArrayInputStream
-import java.io.InputStream
-import java.net.HttpURLConnection
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 class UrlSGVActivity : AppCompatActivity(R.layout.activity_url_svg) {
 
@@ -40,7 +29,7 @@ class UrlSGVActivity : AppCompatActivity(R.layout.activity_url_svg) {
             val pictureDrawable = PictureDrawable(picture)
 
             runOnUiThread {
-                imageView.setImageDrawable(pictureDrawable)
+                Glide.with(this).load(pictureDrawable.current).into(imageView)
             }
         }.start()
     }
