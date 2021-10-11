1d2271b3aed7d29efb309eb0031fe805e94a2b63
Parkduksung <duksung1234@naver.com>
2021-10-11 23:51:35 +0900

    [location] FusedLocationProviderClient, CancellationTokenSource 생성.

--- location/src/main/java/com/example/location/MainActivity.kt
+++ location/src/main/java/com/example/location/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.location.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val fusedLocationProviderClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(applicationContext)
+    }
+
+    private var cancellationTokenSource = CancellationTokenSource()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cancellationTokenSource.cancel()
     }
 }
