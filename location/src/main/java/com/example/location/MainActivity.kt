63d5582852af917fee6f5bdfb5280adf4a7fe02f
Parkduksung <duksung1234@naver.com>
2021-10-15 23:08:54 +0900

    [location] 권한 가져오기 적용.

--- location/src/main/java/com/example/location/MainActivity.kt
+++ location/src/main/java/com/example/location/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.location
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.viewbinding.BuildConfig
 import com.example.location.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +39,93 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         cancellationTokenSource.cancel()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> {
+                    Toast.makeText(this, "권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(this, "현재 위치를 가져올 권한이 생겼습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                else -> {
+
+                    val uri = Uri.fromParts(
+                        "package",
+                        "com.example.location",
+                        null
+                    )
+
+                    val intent = Intent().apply {
+                        action = Settings.ACTION_APPLICATION_SETTINGS
+                        data = uri
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                    startActivity(intent)
+                }
+
+            }
+        }
+    }
+
+    fun locationRequestOnClick(view: View) {
+
+        val permissionApproved =
+            applicationContext.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionApproved) {
+            requestCurrentLocation()
+        } else {
+            requestPermissionWithRationale(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestCurrentLocation() {
+        Log.d(TAG, "requestCurrentLocation()")
+        if (applicationContext.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            val currentLocationTask: Task<Location> =
+                fusedLocationProviderClient.getCurrentLocation(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY,
+                    cancellationTokenSource.token
+                )
+
+            currentLocationTask.addOnCompleteListener { task: Task<Location> ->
+                val result = if (task.isSuccessful && task.result != null) {
+                    val result: Location = task.result
+                    "Location (success): ${result.latitude}, ${result.longitude}"
+                } else {
+                    val exception = task.exception
+                    "Location (failure): $exception"
+                }
+
+                Log.d(TAG, "getCurrentLocation() result: $result")
+                logOutputToScreen(result)
+            }
+        }
+    }
+
+    private fun logOutputToScreen(outputString: String) {
+        val finalOutput = binding.output.text.toString() + "\n" + outputString
+        binding.output.text = finalOutput
+    }
+
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE = 34
     }
 }
