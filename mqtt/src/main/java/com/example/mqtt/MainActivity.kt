7cc4200c832440af83ab630418d224721fb12682
Parkduksung <duksung1234@naver.com>
2021-12-10 16:58:42 +0900

    [mqtt] publish,subscribe 추가.

--- mqtt/src/main/java/com/example/mqtt/MainActivity.kt
+++ mqtt/src/main/java/com/example/mqtt/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mqtt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +16,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        mainViewModel.connectClient { isSuccess: Boolean, message: String ->
-            if(isSuccess) {
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-            }else{
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.connection -> {
+                mainViewModel.connectClient { state, message ->
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+                true
             }
+
+            R.id.subscribe -> {
+                mainViewModel.subscribe { state, message ->
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+                true
+            }
+
+            R.id.publish -> {
+                mainViewModel.publish(msg = "Test") { state, message ->
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return super.onContextItemSelected(item)
     }
 }
--- mqtt/src/main/java/com/example/mqtt/MainViewModel.kt
+++ mqtt/src/main/java/com/example/mqtt/MainViewModel.kt
@@ -55,8 +55,56 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
         }
     }
 
+    fun subscribe(
+        topic: String = DEFAULT_TOPIC,
+        qos: Int = 1,
+        callback: (Boolean, String) -> Unit
+    ) {
+        try {
+            mqttAndroidClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    callback(true, "MqttSubscribe Success")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    callback(false, "MqttSubscribe Fail")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+            callback(false, "MqttException")
+        }
+    }
+
+    fun publish(
+        topic: String = DEFAULT_TOPIC,
+        msg: String,
+        qos: Int = 1,
+        callback: (Boolean, String) -> Unit
+    ) {
+        try {
+            val message = MqttMessage()
+            message.payload = msg.toByteArray()
+            message.qos = qos
+            message.isRetained = false
+            mqttAndroidClient.publish(topic, message, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    callback(true, "MqttPublish Success")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    callback(false, "MqttPublish Fail")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+            callback(false, "MqttException")
+        }
+    }
+
 
     companion object {
         private const val MQTT_BASE_ADDRESS = "tcp://broker.hivemq.com:1883"
+        private const val DEFAULT_TOPIC = "mqttExample"
     }
 }
