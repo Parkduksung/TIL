b747bc833db079b84002e293ff114b0caa5caa41
Parkduksung <duksung1234@naver.com>
2022-03-04 14:57:47 +0900

    [feature] 2초뒤에 Toast Message 가 보여지게끔 구현.

--- flow/src/main/java/com/example/flow/MainActivity.kt
+++ flow/src/main/java/com/example/flow/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.CheckResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<EditText>(R.id.et_input).textChanges().debounce(2000).onEach {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }.launchIn(
+            MainScope()
+        )
     }
+}
+
+
+@ExperimentalCoroutinesApi
+@CheckResult
+fun EditText.textChanges(): Flow<CharSequence?> {
+    return callbackFlow {
+        val listener = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) = Unit
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
+                Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                trySend(s)
+            }
+        }
+        addTextChangedListener(listener)
+        awaitClose { removeTextChangedListener(listener) }
+    }.onStart { emit(text) }
 }
