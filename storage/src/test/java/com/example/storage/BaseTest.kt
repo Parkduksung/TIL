4059f873fb79083b4bec3fea52290ccfdf6d9542
Parkduksung <duksung1234@naver.com>
2022-07-11 19:43:17 +0900

    [storage] print test

---
+++ storage/src/test/java/com/example/storage/BaseTest.kt
@@ -0,0 +1,21 @@
+package com.example.storage
+
+import org.junit.After
+import org.junit.Before
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+abstract class BaseTest {
+    protected val standardOut: PrintStream? = System.out
+    protected val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream()
+
+    @Before
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @After
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+}
--- storage/src/test/java/com/example/storage/ExampleUnitTest.kt
+++ storage/src/test/java/com/example/storage/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package com.example.storage
 
 import android.os.Environment
+import android.util.Log
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,7 +13,7 @@ import java.io.File
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ExampleUnitTest : BaseTest() {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
@@ -37,4 +38,11 @@ class ExampleUnitTest {
         assertEquals(file.path, mockFile.path)
 
     }
+
+    @Test
+    fun `로그가 잘 나오는지 확인한다`() {
+        print("확인")
+        assertEquals("확인", outputStreamCaptor.toString())
+    }
+
 }
