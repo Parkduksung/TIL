3afbce56db73dd6a9b56df4e0d74ff8085d8d5a1
Parkduksung <duksung1234@naver.com>
2022-07-11 17:58:17 +0900

    [storage] 파일이 잘 만들어지는지 testCase 추가.

--- storage/src/test/java/com/example/storage/ExampleUnitTest.kt
+++ storage/src/test/java/com/example/storage/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.storage
 
+import android.os.Environment
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,25 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+
+    @Test
+    fun `파일 경로가 잘 만들어지는지 확인한다`() {
+
+        val fileName = "RemoteViewLog"
+
+        val mockFile = Mockito.mock(File::class.java)
+
+        val documentPath =
+            "/documents/"
+
+        Mockito.`when`(mockFile.path)
+            .thenReturn(documentPath + fileName)
+
+        val file = File(documentPath, fileName)
+
+        assertEquals(file.path, mockFile.path)
+
     }
 }
