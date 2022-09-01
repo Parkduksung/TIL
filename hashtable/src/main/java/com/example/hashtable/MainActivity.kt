227412c0880f5364f31bfbdf709eb0e6cce37dfa
Parkduksung <duksung1234@naver.com>
2022-09-01 23:54:31 +0900

    init timer set

--- hashtable/src/main/java/com/example/hashtable/MainActivity.kt
+++ hashtable/src/main/java/com/example/hashtable/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.hashtable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //시간초 재는 함수.
+        val elapsedTime = measureTimeMillis {
+
+        }
+        println(elapsedTime)
     }
 }
