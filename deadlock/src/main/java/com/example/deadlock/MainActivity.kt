fc9d4ddff662153e18d5e71ebf354743acd22ff4
Parkduksung <duksung1234@naver.com>
2022-10-06 22:48:00 +0900

    DeadLock Coroutine

--- deadlock/src/main/java/com/example/deadlock/MainActivity.kt
+++ deadlock/src/main/java/com/example/deadlock/MainActivity.kt
@@ -2,16 +2,28 @@ package com.example.deadlock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.runBlocking
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("결과,", "start test of coroutines")
         runBlocking {
-            DeadLock.startThread()
+            val job = launch {
+                runBlocking {
+                    println("await cancellation")
+                    awaitCancellation()
+                }
+            }
+            Log.d("결과,","launched job")
+            delay(100)
+            Log.d("결과,","waited a bit")
+            job.cancelAndJoin()
+            Log.d("결과,","canceled and joined")
         }
+        Log.d("결과,", "end test of coroutines")
     }
-
 }
