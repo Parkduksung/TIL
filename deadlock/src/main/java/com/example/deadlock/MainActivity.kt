1734d3e670a56653c6597f616b01c4c5b3c3e76a
Parkduksung <duksung1234@naver.com>
2022-10-12 13:55:26 +0900

    [feature] init realm

--- deadlock/src/main/java/com/example/deadlock/MainActivity.kt
+++ deadlock/src/main/java/com/example/deadlock/MainActivity.kt
@@ -3,27 +3,96 @@ package com.example.deadlock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import kotlinx.coroutines.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val MIN_FRAME_INTERVAL: Long = 1000 / 20
+
+    private var sampleThread: SampleThread? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("결과,", "start test of coroutines")
-        runBlocking {
-            val job = launch {
-                runBlocking {
-                    println("await cancellation")
-                    awaitCancellation()
+
+
+        findViewById<Button>(R.id.btn_start).setOnClickListener {
+            sampleThread = SampleThread().apply {
+                start()
+                Global.setThread(this)
+                SampleRunnable().run()
+            }
+        }
+
+
+        findViewById<Button>(R.id.btn_interrupt).setOnClickListener {
+            release()
+        }
+
+    }
+
+    @Synchronized
+    private fun release() {
+        if (sampleThread?.isAlive == true) {
+            Log.d("결과", "여기탐?")
+            sampleThread?.interrupt()
+            sampleThread?.join()
+            sampleThread = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        release()
+    }
+
+    inner class SampleThread : Thread() {
+
+        override fun run() {
+            Log.d("결과", "SampleThread Run")
+
+            try {
+                while (!isInterrupted) {
+                    synchronized(this@MainActivity) {
+
+
+                    }
+
+                    sleep(MIN_FRAME_INTERVAL)
+                }
+            } catch (e: InterruptedException) {
+                Log.d("결과", "SampleThread InterruptedException")
+            }
+            Log.d("결과", "SampleThread Terminate")
+        }
+    }
+
+
+    inner class SampleRunnable : java.lang.Runnable {
+
+        override fun run() {
+            while (Global.getThread()?.isInterrupted == true) {
+                synchronized(this@MainActivity) {
+                    Log.d("결과", "SampleRunnable run")
                 }
             }
-            Log.d("결과,","launched job")
-            delay(100)
-            Log.d("결과,","waited a bit")
-            job.cancelAndJoin()
-            Log.d("결과,","canceled and joined")
         }
-        Log.d("결과,", "end test of coroutines")
     }
+
 }
+
+object Global {
+
+    private var sampleThread: MainActivity.SampleThread? = null
+
+    fun setThread(thread: MainActivity.SampleThread) {
+        Log.d("결과", "Global setThread")
+        sampleThread = thread
+    }
+
+    fun getThread(): MainActivity.SampleThread? = sampleThread
+
+}
+
